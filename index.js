722e6fc1302f260ced8da154a3535c032e4c1e7c
sanskriti2004 <sanskritigogoi2004@gmail.com>
2025-03-07 20:57:35 +0530

    added routing

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-const express = require("express");
-const app = express();
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
-app.listen(3000);
+http
+  .createServer((req, res) => {
+    const requiredPath = url.parse(req.url).pathname;
+
+    if (requiredPath === "/") {
+      fs.readFile("./index.html", (error, data) => {
+        res.end(data);
+      });
+    } else if (requiredPath === "/about-me") {
+      fs.readFile("./about-me.html", (error, data) => {
+        res.end(data);
+      });
+    } else if (requiredPath === "/contact-me") {
+      fs.readFile("./contact-me.html", (error, data) => {
+        res.end(data);
+      });
+    } else {
+      fs.readFile("./404.html", (error, data) => {
+        res.end(data);
+      });
+    }
+  })
+  .listen(8000);
