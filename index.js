9daee09c988d4c3260acfb012e652f529332da28
sanskriti2004 <sanskritigogoi2004@gmail.com>
2025-03-07 20:46:34 +0530

    added htmls

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.end("Hello World");
-});
-
-server.listen(8080, () => {
-  console.log("Server is running on port 3000");
-});
+app.listen(3000);
